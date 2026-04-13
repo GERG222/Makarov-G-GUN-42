a111d33a32cbf44148d5f8907bebb8e6384624e7
GERG222 <gerg2@yandex.ru>
2026-04-13 16:06:01 +0300

    homework-5

--- Program.cs
+++ Program.cs
@@ -1,39 +1,42 @@
 ﻿using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        if (!Int32.TryParse(Console.ReadLine(), out var a) || !Int32.TryParse(Console.ReadLine(), out var b))
+        // === ЗАДАЧА А ===
+        // Задание 1: Массив из 8 чисел Фибоначчи
+        int[] fibonacci = new int[8];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        for (int i = 2; i < fibonacci.Length; i++)
         {
-            Console.WriteLine("Not a number");
-            return;
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
         }
-
-        var s = Console.ReadLine();
-        if (string.IsNullOrEmpty(s))
-        {
-            Console.WriteLine("Wrong sign");
-            return;
-        }
-
-        int result;
-        switch (s[0])
-        {
-            case '+': result = a + b; break;
-            case '-': result = a - b; break;
-            case '*': result = a * b; break;
-            case '/':
-                if (b == 0) { Console.WriteLine("Division by zero"); return; }
-                result = a / b;
-                break;
-            default:
-                Console.WriteLine("Wrong sign");
-                return;
-        }
-
-        Console.WriteLine($"Decimal:     {result}");
-        Console.WriteLine($"Hexadecimal: {Convert.ToString(result, 16).ToUpper()}");
-        Console.WriteLine($"Binary:      {Convert.ToString(result, 2)}");
+        // Задание 2: Массив названий 12 месяцев
+        string[] months = {
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        };
+        // Задание 3: Матрица 3x3 со степенями чисел 2, 3 и 4
+        int[,] matrix = {
+           { (int)Math.Pow(2, 1), (int)Math.Pow(3, 1), (int)Math.Pow(4, 1) },
+           { (int)Math.Pow(2, 2), (int)Math.Pow(3, 2), (int)Math.Pow(4, 2) },
+           { (int)Math.Pow(2, 3), (int)Math.Pow(3, 3), (int)Math.Pow(4, 3) }
+        };
+        // Задание 4: Ступенчатый (jagged) массив
+        double[][] jaggedArray = new double[3][];
+        jaggedArray[0] = new double[] { 1, 2, 3, 4, 5 };
+        jaggedArray[1] = new double[] { Math.E, Math.PI };
+        jaggedArray[2] = new double[] { Math.Log10(1), Math.Log10(10), Math.Log10(100), Math.Log10(1000) };
+        // === ЗАДАЧА Б ===
+        int[] array = { 1, 2, 3, 4, 5 };
+        int[] array2 = { 7, 8, 9, 10, 11, 12, 13 };
+        // Задание 5: Копирование 3-х элементов
+        Array.Copy(array, array2, 3);
+        // Задание 6: Изменение размера массива в 2 раза через ref
+        int newSize = array.Length * 2;
+        Array.Resize(ref array, newSize);
+        Console.WriteLine("Все задания выполнены успешно!");
     }
 }
